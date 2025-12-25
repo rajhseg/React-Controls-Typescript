1277d9c57bb8a01368ab58af3fc296446f6d4ad2
Rajesh <rajhseg@gmail.com>
2025-12-25 07:01:46 +0530

    RDonutChart
    RDonutChart

--- react-controls-typescript/src/App.tsx
+++ react-controls-typescript/src/App.tsx
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import RButton from './RButton/RButton';
+import RDonutChart, { RDonutChartItem } from './RDonutChart/RDonutChart';
+
 
 function App() {
 
+  
+  const [chartItems, setChartItems] = useState<RDonutChartItem[]>([]);
+
+  
+  const [ButtonHeight, setButtonHeight] = useState('32px');
+
   const handleSubmit = (e:React.MouseEvent<HTMLButtonElement>) => {
-    alert("Hi");
+
+      let pieItem1 = new RDonutChartItem(24,'Batminton', 'darkgreen', 'white');
+      setChartItems((prev)=> [...prev, pieItem1]);
+
+      setButtonHeight((prevState) => '40px');
   }
+
+  const CreateDonutItems = () => {
+
+    let pieItem1 = new RDonutChartItem(24,'Cricket', 'grey', 'white');
+    let pieItem2 = new RDonutChartItem(35,'Volleyball', 'purple', 'white');
+    let pieItem3 = new RDonutChartItem(12,'Tennis', 'gray', 'white');
+    let pieItem4 = new RDonutChartItem(44,'BaseBall', 'teal', 'white');
+    let pieItem5 = new RDonutChartItem(14,'Hockey', 'darkblue', 'white');
+    let pieItem6 = new RDonutChartItem(44,'Football', '#13297A', 'white');
+    
+    setChartItems((prev)=>[]);
+
+    let donutItems: RDonutChartItem[] = [];
+    
+    donutItems.push(pieItem1);
+    donutItems.push(pieItem2);
+    donutItems.push(pieItem3);
+    donutItems.push(pieItem4);
+    donutItems.push(pieItem5);
+    donutItems.push(pieItem6);
+
+    setChartItems((prev)=> [...prev, ...donutItems]);
+  }
+
+  
+  useEffect(()=>{
+    CreateDonutItems();
+  }, []);
 
   return (
     <>
-      <RButton onClick={(e:React.MouseEvent<HTMLButtonElement>)=> handleSubmit(e)}>Submit</RButton>
+      <RButton ButtonHeight={ButtonHeight} onClick={(e:React.MouseEvent<HTMLButtonElement>)=> handleSubmit(e)}>Submit</RButton>
+    
+      <RDonutChart DataListHeight={100} ChartWidth={300} ShadowColor={'blue'} ChartItems={chartItems} Opacity={'0.8'}></RDonutChart>
+      
     </>
   );
 
---
+++ react-controls-typescript/src/RDonutChart/RDonutChart.tsx
@@ -0,0 +1,263 @@
+import { useEffect, useId, useRef, useState } from "react";
+import PropTypes from "prop-types";
+
+import './RDonutChart.css';
+
+export class RDonutChartItem {
+  constructor(
+    public Value: number,
+    public Title: string,
+    public BackgroundColor: string,
+    public ForeColor: string) {
+
+  }
+}
+
+class RRenderDonutChartItem extends RDonutChartItem {
+   
+   Percentage = 0;
+
+   constructor(value: number, title: string, backgroundColor: string, forecolor: string){
+    super(value, title, backgroundColor, forecolor);
+   }
+
+  ConvertToRenderItem(item: RDonutChartItem) {
+    this.Value = item.Value;
+    this.Title = item.Title;
+    this.BackgroundColor = item.BackgroundColor;
+    this.ForeColor = item.ForeColor;
+  }
+
+  ConverToItem() {
+    return new RDonutChartItem(this.Value, this.Title, this.BackgroundColor, this.ForeColor);
+  }
+}
+
+type Props = {
+    FontSize?: number,
+    TextForeColor?: string,
+    RotateTextToInlineAngle?: boolean,
+    ShowTextOnTopOfChartItem?: boolean,
+    MoveTextUpwardsFromCenterInPx?: number,
+    ChartWidth?: number,
+    DataListHeight?: number,
+    ShadowColor?: string,
+    ShadowBlur?: number,
+    Opacity?: string,
+    ChartItems: RDonutChartItem[]
+
+}
+
+const RDonutChart = ({
+    FontSize = 10,
+    TextForeColor = 'white',
+    RotateTextToInlineAngle = false,
+    ShowTextOnTopOfChartItem = true,
+    MoveTextUpwardsFromCenterInPx = 0,
+    ChartWidth = 200,
+    DataListHeight = 100,
+    ShadowColor = 'blue',
+    ShadowBlur = 10,
+    Opacity = '1',
+    ChartItems = []
+}: Props) => {
+
+    const [IsRendered, setIsRendered] = useState(false);
+    const [RenderItems, setRenderItems] = useState<RRenderDonutChartItem[]>([]);
+
+    let _lineWidth: number = 0;
+    
+    let context: CanvasRenderingContext2D | null = null;
+  
+    const Id: string = useId();
+
+    const HostElementId : string = useId();
+
+    const progressCanvas = useRef<HTMLCanvasElement|null>(null);
+
+    const LineWidth = () => {
+        _lineWidth = (ChartWidth / 3) - 12;
+        return _lineWidth;
+    }
+
+    useEffect(()=>{
+        
+        setRenderItems((prevState) => []);
+        let _itms: RRenderDonutChartItem[] = [];
+
+        if (ChartItems) {
+            for (let index = 0; index < ChartItems.length; index++) {
+                const element = ChartItems[index];
+                let itm = new RRenderDonutChartItem(element.Value, element.Title, element.BackgroundColor, element.ForeColor);
+               _itms.push(itm);
+            }
+
+            setRenderItems((prevState)=> [...prevState, ..._itms]);
+            
+         }
+
+    }, [
+        ChartItems,
+        FontSize,
+        TextForeColor,
+        RotateTextToInlineAngle,
+        ShowTextOnTopOfChartItem,
+        MoveTextUpwardsFromCenterInPx,
+        ChartWidth,
+        DataListHeight,
+        ShadowColor,
+        ShadowBlur,
+        Opacity    
+    ]);
+
+    useEffect(()=>{
+
+        if(progressCanvas.current == null)
+            return;
+
+        context = progressCanvas.current.getContext('2d');
+        RenderChart();   
+
+    }, [RenderItems])
+
+    const Items = () => {
+        return RenderItems.map(x => x.ConverToItem());
+    }
+
+        
+    const GetXYForText = (x: number, y: number, length: number, angle: number): { X: number, Y: number } => {
+        let x2 = x + length * Math.cos(angle);
+        let y2 = y + length * Math.sin(angle);
+        return { X: x2, Y: y2 };
+    }
+
+    const DrawText = (context: CanvasRenderingContext2D, x: number, y: number, length: number, angle: number, color: string) => {
+        let rad = (angle * Math.PI) / 180;
+        context.beginPath();
+        context.moveTo(x, y);
+        context.lineTo(x + length * rad, y + length * rad);
+        context.strokeStyle = color;
+        context.stroke();
+        context.closePath();
+    }
+
+    const RenderChart = () => {
+
+        setIsRendered((prevState) => false);
+
+        if (progressCanvas && progressCanvas.current && context && RenderItems.length > 0) {
+
+            context.clearRect(0, 0, progressCanvas.current.width, progressCanvas.current.height);
+            
+            let x = ChartWidth / 2;
+            let y = ChartWidth / 2;
+            let radiusLength = (ChartWidth / 3) + (LineWidth() / 2);
+
+            let totalValues = RenderItems.map(x => x.Value);
+            let TotalCount = 0;
+
+            for (let index = 0; index < totalValues.length; index++) {
+                const element = totalValues[index];
+                TotalCount = TotalCount + element;
+            }
+
+            let start = 0 * Math.PI / 180;
+            let previousAngle = start;
+
+            for (let index = 0; index < RenderItems.length; index++) {
+                const element = RenderItems[index];
+
+                let percentage = (element.Value * 100) / TotalCount;
+                let end1 = (percentage / 100) * 359.98 * (Math.PI / 180);
+
+                element.Percentage = percentage;
+                context.fillStyle = element.BackgroundColor;
+                context?.beginPath();
+                context.lineWidth = LineWidth();
+
+                context?.arc(x, y, ChartWidth / 3, previousAngle, (previousAngle + end1), false);
+                context.strokeStyle = element.BackgroundColor;
+
+                context.shadowBlur = ShadowBlur;
+                context.shadowColor = ShadowColor;
+                context?.stroke();
+                context.closePath();
+
+                if (ShowTextOnTopOfChartItem) {
+                let endAngle = previousAngle + end1;
+                let avgAngle = (previousAngle + (endAngle > previousAngle ? endAngle : endAngle + ((Math.PI * 359.58) / 180))) / 2;
+
+                let metrics = context.measureText(element.Title);
+                let textRadiusLength = radiusLength - metrics.width - 5 + MoveTextUpwardsFromCenterInPx;
+
+                let pos = GetXYForText(x, y, textRadiusLength, avgAngle);
+
+                context.beginPath();
+
+                context.save();
+                context.textAlign = "center";
+                context.textBaseline = "middle";
+
+                context.translate(pos.X, pos.Y);
+
+                if (RotateTextToInlineAngle) {
+                    context.rotate(avgAngle);
+                } else {
+                    context.rotate(Math.PI / 2);
+                }
+
+                context.font = FontSize + 'px verdana';
+                context.fillStyle = TextForeColor;
+                context.fillText(element.Title, 0, 0);
+
+                context.stroke();
+                context.restore();
+                context.closePath();
+
+                }
+
+                previousAngle = previousAngle + end1;
+            }
+
+            progressCanvas.current.style.transform = "rotate(-90deg)";
+            progressCanvas.current.style.opacity = Opacity;
+        
+            setIsRendered((prevState) => true);
+    
+        }
+    }
+
+
+    return (
+        <>
+        <div id={HostElementId} className="host">
+            <div id={Id} style={{position: 'relative', width: (ChartWidth+'px'), height: ((ChartWidth + DataListHeight) +'px') }}>
+                <canvas style={{position: 'absolute'}} ref={progressCanvas} width={ChartWidth} height={ChartWidth} 
+                    className="canvasCenter">
+
+                </canvas>    
+                {
+                    IsRendered  &&
+                        <div style={{position: 'relative', bottom:-ChartWidth+'px', height: DataListHeight+'px' }}>
+                            <div className="dataContainer">
+                                {
+                                    RenderItems.map((itm, index) => (
+                                        <div className="data" key={index}>
+                                            <div className="indicator" style={{backgroundColor: itm.BackgroundColor}}>                    
+                                            </div>
+                                            <span className="title" style={{width: '50px'}}>{itm.Title}</span>
+                                            <span className="title" style={{width: '30px'}}>({itm.Value})</span>
+                                        </div>
+                                    ))
+                                }
+                                
+                            </div>        
+                        </div>
+                }
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default RDonutChart;
